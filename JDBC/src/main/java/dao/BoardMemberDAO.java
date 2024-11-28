969b4d9554f40d9345c9cab827eec819a1759a70
nam2626 <nam2626@naver.com>
2024-11-28 16:53:48 +0900

    전체 조회

--- JDBC/src/main/java/dao/BoardMemberDAO.java
+++ JDBC/src/main/java/dao/BoardMemberDAO.java
@@ -1,7 +1,12 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import dto.BoardMemberDTO;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class BoardMemberDAO {
@@ -26,5 +31,44 @@ public class BoardMemberDAO {
 		return instance;
 	}
 
+	public ArrayList<BoardMemberDTO> selectAllMember() {
+		ArrayList<BoardMemberDTO> list = new ArrayList<BoardMemberDTO>();
+		//1. SQL 문작성
+		String sql = "select * from board_member";
+		//2. Connection 받아옴
+		//3. PreparedStatement 생성
+		try (Connection conn = ods.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);){
+			//4. SQL문 실행
+			try(ResultSet rs = pstmt.executeQuery();){
+				//5. 결과처리
+				while(rs.next()) {
+					BoardMemberDTO dto = new BoardMemberDTO();
+					dto.setId(rs.getString("id"));
+					dto.setPassword(rs.getString("password"));
+					dto.setNickName(rs.getString("nickname"));
+					dto.setUserName(rs.getString("username"));
+					
+					list.add(dto);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		//6. 결과값 리턴
+		return list;
+	}
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
