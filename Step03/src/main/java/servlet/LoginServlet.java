069cb6cd5348cf5e929fcf0fdbf93f17d093e769
nam2626 <nam2626@naver.com>
2024-11-28 14:07:32 +0900

    로그인 처리

---
+++ Step03/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		//파라미터 받아옴
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		//처리
+		String path = "03_login.jsp";
+		if(id.equals("admin") && passwd.equals("123456")) {
+			path = "login_result.jsp";
+			session.setAttribute("msg", "로그인에 성공하셨습니다.");
+		}
+		//페이지 이동
+		response.sendRedirect(path);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
