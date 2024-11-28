ea3c154b1526935b6cf734fbc89bd354b36d8ef7
nam2626 <nam2626@naver.com>
2024-11-28 11:50:39 +0900

    session 준비

---
+++ Step03/src/main/java/servlet/SessionServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/session.do")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//세션 정보 읽어옴
+		HttpSession session = request.getSession();
+		String msg = request.getParameter("msg");
+		
+		session.setAttribute("msg", msg);
+		//세션 유효시간 초단위
+//		session.setMaxInactiveInterval(1800);
+		
+		//페이지 이동 session_result.jsp
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
