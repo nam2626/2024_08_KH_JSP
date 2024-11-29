6dcf27e9fe6da8c738ba3f5b3da856335dabe899
nam2626 <nam2626@naver.com>
2024-11-29 10:26:20 +0900

    open session

---
+++ MyBatis1/src/main/java/config/DBManager.java
@@ -0,0 +1,41 @@
+package config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBManager {
+	private static DBManager instance = new DBManager();
+	private SqlSessionFactory sqlSessionFactory;
+	private DBManager() {
+		String resource = "org/mybatis/example/mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			System.out.println("DB 셋팅 완료");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DBManager getInstance() {
+		if(instance==null)
+			instance = new DBManager();
+		return instance;
+	}
+	
+	//Connection 하나 리턴
+	public SqlSession getSession() {
+		//true - auto commit, false - commit
+		return sqlSessionFactory.openSession(true);
+	}
+	
+	
+	
+	
+}
