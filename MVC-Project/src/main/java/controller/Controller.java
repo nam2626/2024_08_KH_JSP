bdbf29edd9d1aa32c34dcace3e8ed14714cf14c3
nam2626 <nam2626@naver.com>
2024-12-05 11:02:28 +0900

    Controller 생성

---
+++ MVC-Project/src/main/java/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface Controller {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
--- MVC-Project/src/main/java/servlet/DispatcherServlet.java
+++ MVC-Project/src/main/java/servlet/DispatcherServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Servlet implementation class DispatcherServlet
@@ -26,9 +27,16 @@ public class DispatcherServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=utf-8");
-		response.getWriter().println(request.getRequestURI() + "<br>");
-		response.getWriter().println(request.getRequestURL() + "<br>");
+//		response.setContentType("text/html;charset=utf-8");
+//		response.getWriter().println(request.getRequestURI() + "<br>");
+//		response.getWriter().println(request.getRequestURL() + "<br>");
+//		String[] path = request.getRequestURI().split("/");
+//		System.out.println(Arrays.toString(path));
+//		System.out.println(path[path.length-1]);
+//		System.out.println(path[path.length-1].substring(0,path[path.length-1].length() -3));
+//		System.out.println(path[path.length-1].replace(".do", ""));
+		String[] path = request.getRequestURI().split("/");
+		String command = path[path.length-1].replace(".do", "");
 	
 	}
 
