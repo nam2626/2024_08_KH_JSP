347633f45a1497477657bde9d74d7fe50bb79b17
nam2626 <nam2626@naver.com>
2024-12-10 11:13:21 +0900

    조회한 글번호들 세션처리

--- BoardProject/src/main/java/controller/BoardViewController.java
+++ BoardProject/src/main/java/controller/BoardViewController.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import dto.BoardDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import service.BoardService;
 import view.ModelAndView;
 
@@ -17,7 +19,17 @@ public class BoardViewController implements Controller {
 		//글번호 받아오기
 		int bno = Integer.parseInt(request.getParameter("bno"));
 		//글번호에 해당하는 게시글의 조회수 증가
-		BoardService.getInstance().updateBoardCount(bno);
+		//Set 하나를 세션에 저장해놓고, 방문한적이 있는 게시글 번호를 추가
+		//추가 결과가 true일때만 조회수 증가처리
+		HttpSession session = request.getSession();
+		HashSet<Integer> history = (HashSet<Integer>) session.getAttribute("history");
+		if(history == null) {
+			history = new HashSet<Integer>();
+			session.setAttribute("history", history);
+		}
+		System.out.println(history);
+		if(history.add(bno))
+			BoardService.getInstance().updateBoardCount(bno);
 		//글번호에 게시글 조회
 		BoardDTO dto = BoardService.getInstance().selectBoard(bno);
 		//해당 게시글의 댓글 목록 조회
