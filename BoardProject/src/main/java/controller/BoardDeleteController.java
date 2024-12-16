d2f5def31fdc8eb839fc546a663bde7e19ccc11c
nam2626 <nam2626@naver.com>
2024-12-16 11:00:29 +0900

    첨부 파일 삭제

--- BoardProject/src/main/java/controller/BoardDeleteController.java
+++ BoardProject/src/main/java/controller/BoardDeleteController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import dto.BoardFileDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,6 +18,16 @@ public class BoardDeleteController implements Controller {
 			throws ServletException, IOException {
 		int bno = Integer.parseInt(request.getParameter("bno"));
 		System.out.println(bno);
+		//-------------------------------------------------------
+		//	1. 해당 게시글의 파일 경로들을 가져옴
+		List<BoardFileDTO> list = BoardService.getInstance().getBoardFileList(bno);
+		//	2. 경로에 해당하는 파일들을 물리적으로 삭제 - File 클래스 이용		
+		list.forEach(item -> {
+			File file = new File(item.getFpath());
+			file.delete();//물리적으로 삭제
+		});
+		
+		//-------------------------------------------------------
 		BoardService.getInstance().deleteBoard(bno);
 		
 		ModelAndView view = new ModelAndView();
